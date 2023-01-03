819a2e3bcc9a771f668d8dc68747dfc6e7f41d65
Vebjørn Nystad <vebjorn.nystad@hotmail.com>
2023-01-03 17:02:18 +0100

    mvp bot

--- src/main/java/socially/disturbed/Main.java
+++ src/main/java/socially/disturbed/Main.java
@@ -2,6 +2,9 @@ package socially.disturbed;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Shall not be pushed to git more than once
+        // To prevent token pushed to git
+        String token = "";
+        Bot bot = new Bot(token);
     }
 }
