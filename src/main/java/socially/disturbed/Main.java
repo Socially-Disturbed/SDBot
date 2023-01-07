ab9ce3958c38cae96a44da12836381ce1224b1a3
Vebjørn Nystad <vebjorn.nystad@hotmail.com>
2023-01-08 00:09:11 +0100

    getting token from env variable

--- src/main/java/socially/disturbed/Main.java
+++ src/main/java/socially/disturbed/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         // Shall not be pushed to git more than once
         // To prevent token pushed to git
-        String token = "MTA1OTUyNTMyMjE5NTg3Mzk1NA.Gk7i7M.l8NLHYbs0JuwEBNmxwezqIz-HdHl2nlew4SXUI";
+        String token = System.getenv("discordToken");
         DbService dbs = new DbService();
         Bot bot = new Bot(token, dbs);
     }
