b6dd8e04387c0d6dcc0bb2767354aef67ca0dec5
Vebjørn Nystad <vebjorn.nystad@hotmail.com>
2023-01-21 22:14:48 +0100

    db script

--- src/main/resources/dbsetup.sql
+++ src/main/resources/dbsetup.sql
@@ -1,11 +1,25 @@
+-- Table: public.SD_GUEST_SCORE
+
+-- DROP TABLE IF EXISTS public."SD_GUEST_SCORE";
+
 CREATE TABLE IF NOT EXISTS public."SD_GUEST_SCORE"
 (
     "NAME" text COLLATE pg_catalog."default" NOT NULL,
     "SCORE" numeric,
     "WINS" integer,
     "ADR" integer,
+    "RANK" text COLLATE pg_catalog."default",
     CONSTRAINT "SD_GUEST_SCORE_pkey" PRIMARY KEY ("NAME")
 )
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public."SD_GUEST_SCORE"
+    OWNER to postgres;
+
+-- Table: public.SD_SCORE
+
+-- DROP TABLE IF EXISTS public."SD_SCORE";
 
 CREATE TABLE IF NOT EXISTS public."SD_SCORE"
 (
@@ -13,5 +27,11 @@ CREATE TABLE IF NOT EXISTS public."SD_SCORE"
     "SCORE" numeric,
     "WINS" integer,
     "ADR" integer,
+    "RANK" text COLLATE pg_catalog."default",
     CONSTRAINT "SD_SCORE_pkey" PRIMARY KEY ("NAME")
 )
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public."SD_SCORE"
+    OWNER to postgres;
